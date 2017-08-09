83e9369b157e9cc15767aafa0089e8bf6b1d9314
anjana <anjana@easyhire.me>
2017-08-09 08:31:21 -0700

    Adding 'ragas' directory that contains the Alexa skills called 'ragas'

---
+++ ragas/lambda/index.js
@@ -0,0 +1,14 @@
+var Alexa = require('alexa-sdk');
+
+exports handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+
+    'LaunchRequest': function() {
+        this.emit(':ask', 'Welcome to Ragas', 'I ')
+    }
+}
