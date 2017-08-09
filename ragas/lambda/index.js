6c2a8cc25aab962a395a10b355e6169a9bbb87e9
anjana <anjana@easyhire.me>
2017-08-09 11:14:54 -0700

    Created the alexa raga invocation

--- ragas/lambda/index.js
+++ ragas/lambda/index.js
@@ -1,6 +1,6 @@
 var Alexa = require('alexa-sdk');
 
-exports handler = function(event, context, callback) {
+exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
     alexa.execute();
@@ -9,6 +9,10 @@ exports handler = function(event, context, callback) {
 var handlers = {
 
     'LaunchRequest': function() {
-        this.emit(':ask', 'Welcome to Ragas', 'I ')
+        this.emit(':ask', 'Welcome to Ragas! Ask me anything about a carnatic raga and I will tell you the origin, ascending and descending sequence called aroehunum and ahvurohunum, and popular kcritheez in that raga', 'Try asking me about a raga');
+    },
+
+    'Hello': function() {
+        this.emit(':tell', 'Hi there');
     }
-}
+};
